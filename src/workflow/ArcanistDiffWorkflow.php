e515ccfc478410b62ddc938a99a6921f6a9d61ca
isobel <isobel@thoughtmachine.net>
2026-09-17 15:25:23 +0100

    Sometimes add a reminder that Dingley exists to 'arc diff'

--- src/workflow/ArcanistDiffWorkflow.php
+++ src/workflow/ArcanistDiffWorkflow.php
@@ -592,6 +592,14 @@ EOTEXT
       if ($this->shouldOpenCreatedObjectsInBrowser()) {
         $this->openURIsInBrowser(array($uri));
       }
+      if (mt_rand(1, 100) <= 20) {
+        echo phutil_console_format(
+          "<fg:yellow>**%s**</fg> %s\n",
+          pht('TOP TIP!'),
+          pht(
+            'Let Dingley review your PR before raising it. Start gemini '.
+            'and type \'Use your Dingley skill to review my changes\'.'));
+      }
     }
 
     echo pht('Included changes:')."\n";
